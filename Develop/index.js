f1e4d334525ea980a721a7f84326e722e6b672be
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-07-16 16:54:13 -0700

    added badge response in

--- Develop/index.js
+++ Develop/index.js
@@ -98,7 +98,14 @@ ${response.license}
 
 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
+## Badges
 
+![${response.license}](https://img.shields.io/badge/license-${response.license}-blue)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+## How to Contribute
 
 ${response.repoContrib}
 
