5aad86d9d3c7c9ae05d80ec163bb3f75a68eda54
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-07-16 14:27:05 -0700

    added inquirer prompt

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,32 @@
 const fs = require('file-system');
 const inquirer = require('inquirer');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) => {
+    console.log(response)
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  }
+  );
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
